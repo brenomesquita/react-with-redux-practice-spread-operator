6c6368b034b10b030010aefaceaa8cf80c589541
brenomesquita <brenomesquitaribeiro@gmail.com>
2020-05-27 18:45:47 -0300

    routes

--- src/App.js
+++ src/App.js
@@ -1,10 +1,16 @@
 import React from 'react';
+import { BrowserRouter, Switch, Route } from 'react-router-dom';
+import Home from './pages/Home';
+import Login from './pages/Login';
 
 function App() {
   return (
-    <div>
-      ok
-    </div>
+    <BrowserRouter>
+      <Switch>
+        <Route exact path='/' component={Home} />
+        <Route path='/login' component={Login} />
+      </Switch>
+    </BrowserRouter>
   );
 }
 
